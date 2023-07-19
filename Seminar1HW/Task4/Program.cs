898a6eafffe5d2cbe5aa790e3ae88980ff8c0485
Den <denisshadrin021@gmail.com>
2023-07-19 16:05:24 +0500

    Добавлена 4 задача

---
+++ Seminar1HW/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Из числа N показать все четные числа от 1 до N
+Console.Clear();
+int N = int.Parse(Console.ReadLine());
+int B = 1;
+
+while (B <= Math.Abs(N))
+{
+    int ost = B % 2;
+    if (ost == 0)
+        Console.Write($"{B} ");
+    B++;
+}
