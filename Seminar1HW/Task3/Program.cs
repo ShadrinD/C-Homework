5447b345012b13c798fbae63ed880e7d4a7f0269
Den <denisshadrin021@gmail.com>
2023-07-19 15:55:57 +0500

    Добавлна 3 задача

---
+++ Seminar1HW/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Является ли число четным
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int ost = a % 2;
+if (ost == 0)
+{
+    Console.WriteLine($"{a} - четное число");
+}
+if (ost != 0)
+{
+    Console.WriteLine($"{a} - нечетное число");
+}
