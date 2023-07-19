4f45aa4c785263e8fa3433302f15a7d0759bbfe4
Den <denisshadrin021@gmail.com>
2023-07-19 15:38:50 +0500

    Добавлна 1 задача

---
+++ Seminar1HW/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сравнить, какое из двух чиcел больше
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"{a} - Наибольшее число");
+}
+if (a < b)
+{
+    Console.WriteLine($"{b} - Наибольшее число");
+}
