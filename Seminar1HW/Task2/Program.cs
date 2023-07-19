a81acec59811e0c5b64e66304f3129d50741f89d
Den <denisshadrin021@gmail.com>
2023-07-19 15:47:58 +0500

    Добавлна 2 задача

---
+++ Seminar1HW/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Сравнить, какое из трех чиcел больше
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = 1;
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine($"{max} - Наибольшее число");
