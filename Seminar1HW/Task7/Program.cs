9421209275ae48a58de951938bc6f49c061d8d26
Den <denisshadrin021@gmail.com>
2023-07-19 15:18:06 +0500

    Добавлна 7 задача

---
+++ Seminar1HW/Task7/Program.cs
@@ -0,0 +1,7 @@
+﻿//Получить последнее число от трехзначного
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int y = number % 10;
+
+Console.WriteLine($"{y}");
