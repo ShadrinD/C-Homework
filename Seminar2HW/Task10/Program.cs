07d653745a7ecccf45a8729bfc7b1ba1904f801b
Den <denisshadrin021@gmail.com>
2023-07-27 13:19:37 +0530

    10 и 13 задачи

---
+++ Seminar2HW/Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Принять трехзначное число и выдать его вторую цифру
+Console.Clear();
+int num = new Random() .Next(100, 1000);
+Console.WriteLine(num);
+int result1 = (num % 100 / 10);
+Console.WriteLine(result1);
---
+++ Seminar2HW/Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести 3 цифру заданного числа или сказать что её нет
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num / 100 > 1)
+{
+    int result = (num % 10);
+    Console.WriteLine(result);
+}
+else {
+    Console.WriteLine("Неправильное число");
+    return;
+}
+
