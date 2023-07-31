7c62aa24605a11c668d59cbb2599dc5029876d14
Den <denisshadrin021@gmail.com>
2023-07-31 15:39:01 +0530

    5 семинар дз

--- Seminar5HW/Task1/Program.cs
+++ Seminar5HW/Task1/Program.cs
@@ -4,3 +4,28 @@
 
 [345, 897, 568, 234] -> 2*/
 
+int[] arr = new int[8];
+FillArray(arr,1,999);
+Console.WriteLine(String.Join(" ", arr));
+Console.WriteLine(GetCount(arr));
+
+void FillArray(int[] coll, int min_i, int max_i)
+{
+    for (int i = 0; i < coll.Length; i++)
+    {
+        coll[i] = new Random().Next(min_i, max_i+1);
+    }
+}
+
+int GetCount(int[] arr)
+{
+    int count = 0; 
+   foreach (var item in arr)
+   {
+    if (item%2 == 0)
+    {
+       count++; 
+    }
+   }
+     return  count;
+}
--- Seminar5HW/Task3/Program.cs
+++ Seminar5HW/Task3/Program.cs
@@ -3,3 +3,29 @@
 
 [3 7 22 2 78] -> 76
 */
+
+double[] arr = new double[8];
+FillArray(arr);
+Console.WriteLine(String.Join(" ", arr));
+Console.WriteLine(GetCount(arr));
+
+void FillArray(double[] coll)
+{
+    for (int i = 0; i < coll.Length; i++)
+    {
+        coll[i] = new Random().NextDouble()*10;
+    }
+}
+
+double GetCount(double[] arr)
+{
+    double max_i=Math.Round(arr[0],2), min_i = Math.Round(arr[0],2); 
+   for (int i=1; i<arr.Length; i++ )
+   {
+    arr[i]=Math.Round(arr[i],2); 
+    if (arr[i]>max_i) max_i=arr[i];
+    if (arr[i]<min_i)  min_i=arr[i];
+    Console.WriteLine($"{arr[i]} - {max_i} vs {min_i}"); 
+   }
+     return  Math.Round(max_i-min_i,2);
+}
